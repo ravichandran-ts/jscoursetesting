88822c4b1993af38f1cefc5871052d2e9efbd752
Ravichandran TS <ravichandran.ts@gmail.com>
2026-06-01 03:11:43 -0400

    Included Total Length feature for practice test

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -26,6 +26,7 @@ startTime = new Date().getTime();
     // Calculate time elapsed and words per minute (WPM)
     var timeElapsed = (endTime - startTime) / 1000; // in seconds
     var userTypedText = document.getElementById("userInput").value;
+    var typedLength = userTypedText.length;
 
     // Split the text using regex to count words correctly
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
@@ -41,6 +42,7 @@ startTime = new Date().getTime();
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + typedLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
