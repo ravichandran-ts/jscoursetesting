9f5e919a81f729c5cbc493f7d1f886fad323f04e
Ravichandran TS <ravichandran.ts@gmail.com>
2026-06-04 05:39:43 -0400

    added news Article project folders and files

--- newsArticle/news_article.js
+++ newsArticle/news_article.js
@@ -4,6 +4,8 @@ var url = './news_article.json';
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
 
+//debugger;
+
 xhr.onload = function() {
 var articles = xhr.response.articles;
 var articlesDiv = document.getElementById('articles');
@@ -13,38 +15,56 @@ var articlesDiv = document.getElementById('articles');
         articleDiv.classList.add('article');
 
         var title = document.createElement('h2');
-        title.textContent = article.title;
+        title.textContent = "Title: " + article.title;
+
+        var author = document.createElement('p');
+        author.textContent = "Author: " + article.author;
+
+        var source = document.createElement('p');
+        source.textContent = "Source: " + article.source;
+
+        var category = document.createElement('p');
+        category.textContent = "Category: " + article.category;
+
+        var published = document.createElement('p');
+        published.textContent = "Published: " + article.publishedAt;
 
         var description = document.createElement('p');
-        description.textContent = article.description;
+        description.textContent = "Description: " + article.description;
 
-        var otherHeader = document.createElement('h3');
-        otherHeader.textContent = 'Other Information';
+//        var otherHeader = document.createElement('h3');
+//        otherHeader.textContent = 'Other Information';
 
-        var othList = document.createElement('ul');
-        var listItem = document.createElement('li');
-        listItem.textContent = article.url;
-        othList.appendChild(listItem);
-        var listItem = document.createElement('li');
-        listItem.textContent = article.imageURL;
-        othList.appendChild(listItem);
-        var listItem = document.createElement('li');
-        listItem.textContent = article.imageURL;
-        othList.appendChild(listItem);
-        var listItem = document.createElement('li');
-        listItem.textContent = article.Content;
-        othList.appendChild(listItem);
-        var listItem = document.createElement('li');
-        listItem.textContent = article.tags;
-        othList.appendChild(listItem);
-    });
+//        var arturl = document.createElement('p');
+//        arturl.textContent = article.url;
+
+//        var arturlimg = document.createElement('p');
+//        arturlimg.textContent = article.imageURL;
+
+        var artcontent = document.createElement('p');
+        artcontent.textContent = "Content: " + article.content;
+
+        var arttags = document.createElement('p');
+        arttags.textContent = "Tags: " + article.tags;
+
+        var dashline = document.createElement('p');
+        dashline.textContent = "------------------------------"
 
       articleDiv.appendChild(title);
+      articleDiv.appendChild(author);
+      articleDiv.appendChild(source);
+      articleDiv.appendChild(category);
+      articleDiv.appendChild(published);
       articleDiv.appendChild(description);
-      articleDiv.appendChild(otherHeaderHeader);
-      articleDiv.appendChild(outerHeightList);
-
+//      articleDiv.appendChild(otherHeader);
+//      articlesDiv.appendChild(arturl);
+//      articlesDiv.appendChild(arturlimg);
+      articlesDiv.appendChild(artcontent);
+      articlesDiv.appendChild(arttags);
+      articlesDiv.appendChild(dashline);
       articlesDiv.appendChild(articleDiv);
-};
+      articlesDiv.appendChild(dashline);
+    });
+}
 
 xhr.send();
